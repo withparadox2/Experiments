24a6d34ecbf11af11be8d9bd3b5100314d50c44a
withparadox2 <withparadox2@gmail.com>
2017-08-09 01:08:28 +0800

    Using real hyper-dimen rotation matrix.

--- RotateCube/app/src/main/java/com/withparadox2/rotatecube/CubeRender.java
+++ RotateCube/app/src/main/java/com/withparadox2/rotatecube/CubeRender.java
@@ -14,6 +14,7 @@ import javax.microedition.khronos.opengles.GL10;
 
 import static android.opengl.GLES20.*;
 import static android.opengl.Matrix.multiplyMM;
+import static android.opengl.Matrix.multiplyMV;
 import static android.opengl.Matrix.rotateM;
 import static android.opengl.Matrix.setIdentityM;
 import static android.opengl.Matrix.translateM;
@@ -37,100 +38,66 @@ public class CubeRender implements GLSurfaceView.Renderer {
 
   private static final String U_MATRIX = "u_Matrix";
   private final float[] projectionMatrix = new float[16];
-  private final float[] backMatrix = new float[16];
   private final float[] modelMatrix = new float[16];
 
-  float[] outerPoints = {
-      -1f, -1f, -1f, -1f, 1f, -1f, -1f, 1f, 1f, -1f, -1f, 1f, 1f, -1f, -1f, 1f, 1f, -1f, 1f, 1f, 1f,
-      1f, -1f, 1f,
+  private final float[] hyperMatrix = new float[16];
+
+  private float[][] vertexes = {
+      { -1, -1, -1, -1 }, { -1, -1, -1, 1 }, { -1, -1, 1, -1 }, { -1, -1, 1, 1 }, { -1, 1, -1, -1 },
+      { -1, 1, -1, 1 }, { -1, 1, 1, -1 }, { -1, 1, 1, 1 }, { 1, -1, -1, -1 }, { 1, -1, -1, 1 },
+      { 1, -1, 1, -1 }, { 1, -1, 1, 1 }, { 1, 1, -1, -1 }, { 1, 1, -1, 1 }, { 1, 1, 1, -1 },
+      { 1, 1, 1, 1 }
   };
 
-  float[] backOuterPoints = {
-      -1f, -1f, -1f, -1f, 1f, -1f, -1f, 1f, 1f, -1f, -1f, 1f, 1f, -1f, -1f, 1f, 1f, -1f, 1f, 1f, 1f,
-      1f, -1f, 1f,
+  private int[][] edges = {
+      { 1, 3 }, { 1, 5 }, { 1, 9 }, { 3, 7 }, { 3, 11 }, { 5, 7 }, { 5, 13 }, { 7, 15 }, { 9, 11 },
+      { 9, 13 }, { 11, 15 }, { 13, 15 }, { 2, 4 }, { 2, 6 }, { 2, 10 }, { 4, 8 }, { 4, 12 },
+      { 6, 8 }, { 6, 14 }, { 8, 16 }, { 10, 12 }, { 10, 14 }, { 12, 16 }, { 14, 16 }, { 1, 2 },
+      { 3, 4 }, { 5, 6 }, { 7, 8 }, { 9, 10 }, { 11, 12 }, { 13, 14 }, { 15, 16 }
   };
 
-  float[] innerPoints = {
-      -0.5f, -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f,
-      -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 0.5f, -0.5f, 0.5f,
-  };
+  private float[] newLines = new float[192];//32 * 2 * 3
 
-  float[] backInnerPoints = {
-      -0.5f, -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f,
-      -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 0.5f, -0.5f, 0.5f,
-  };
-
-  float[] lines = new float[144];
+  private float[] points = new float[16 * 3];
 
   public CubeRender(Context context) {
     this.mContext = context;
+    setIdentityM(hyperMatrix, 0);
 
-    //float[] tableVerticesWithTriangles = {
-    //    //下
-    //    -1f, -1f, -1f,
-    //    -1f,  1f, -1f,
-    //     1f, -1f, -1f,
-    //     1f,  1f, -1f,
-    //
-    //    //上
-    //    -1f, -1f,  1f,
-    //    -1f,  1f,  1f,
-    //     1f, -1f,  1f,
-    //     1f,  1f,  1f,
-    //
-    //    //左
-    //    -1f, -1f, -1f,
-    //    -1f, -1f,  1f,
-    //    -1f,  1f, -1f,
-    //    -1f,  1f,  1f,
-    //
-    //    //右
-    //     1f, -1f, -1f,
-    //     1f, -1f,  1f,
-    //     1f,  1f, -1f,
-    //     1f,  1f,  1f,
-    //
-    //    //前
-    //    -1f, -1f, -1f,
-    //    -1f, -1f,  1f,
-    //     1f, -1f, -1f,
-    //     1f, -1f,  1f,
-    //
-    //    //后
-    //    -1f,  1f, -1f,
-    //    -1f,  1f,  1f,
-    //     1f,  1f, -1f,
-    //     1f,  1f,  1f,
-    //};
-
-    fillLines();
-    vertexData = ByteBuffer.allocateDirect(lines.length * BYTES_PER_FLOAT)
+    projection(0f);
+    vertexData = ByteBuffer.allocateDirect(newLines.length * BYTES_PER_FLOAT)
         .order(ByteOrder.nativeOrder())
         .asFloatBuffer();
 
-    vertexData.put(lines);
+    vertexData.put(newLines);
   }
 
-  private void fillLines() {
-    for (int i = 0; i < 8; i++) {
+  private float[] tempArray = new float[4];
+
+  private void projection(float angle) {
+    hyperMatrix[0] = (float) Math.cos(angle);
+    hyperMatrix[3] = (float) -Math.sin(angle);
+    hyperMatrix[12] = (float) Math.sin(angle);
+    hyperMatrix[15] = (float) Math.cos(angle);
+
+    for (int i = 0; i < vertexes.length; i++) {
+      multiplyMV(tempArray, 0, hyperMatrix, 0, vertexes[i], 0);
       for (int j = 0; j < 3; j++) {
-        int index = 3 * i + j;
-        //outer: left and right frame
-        lines[index] = outerPoints[index];
-        //inner: left and right frame
-        lines[48 + index] = innerPoints[index];
+        points[i * 3 + j] = tempArray[j] / (3 - tempArray[3]);
+      }
+    }
+    fillNewLines();
+  }
 
-        int target = 3 * (i / 2) + j;
-        target = i % 2 == 0 ? target : target + 12;
-        //outer: four connectors between left and right
-        lines[24 + index] = outerPoints[target];
-        //inner: four connectors between left and right
-        lines[72 + index] = innerPoints[target];
+  private void fillNewLines() {
+    for (int i = 0; i < edges.length; i++) {
+      int[] e = edges[i];
+      int p1 = (e[0] - 1) * 3;
+      int p2 = (e[1] - 1) * 3;
 
-        //eight connectors between outer and inner
-        target = 6 * i + j;
-        lines[96 + target] = outerPoints[index];
-        lines[96 + target + 3] = innerPoints[index];
+      for (int j = 0; j < 3; j++) {
+        newLines[i * 6 + j] = points[p1 + j];
+        newLines[i * 6 + 3 + j] = points[p2 + j];
       }
     }
   }
@@ -163,217 +130,45 @@ public class CubeRender implements GLSurfaceView.Renderer {
   @Override public void onSurfaceChanged(GL10 gl, int width, int height) {
     glViewport(0, 0, width, height);
 
-    Matrix.perspectiveM(projectionMatrix, 0, 45, (float) width / (float) height, 0.1f, 100f);
+    Matrix.perspectiveM(projectionMatrix, 0, 45, (float) width / (float) height, 0.1f, 20f);
 
     setIdentityM(modelMatrix, 0);
 
-    translateM(modelMatrix, 0, 0f, 0f, -10f);
-    rotateM(modelMatrix, 0, -30f, 0.5f, 1f, 0f);
+    translateM(modelMatrix, 0, 0f, 0f, -5f);
+    rotateM(modelMatrix, 0, 20f, 1f, 0f, 0f);
+    rotateM(modelMatrix, 0, 130f, 0f, 1f, 0f);
 
     multiplyMM(temp, 0, projectionMatrix, 0, modelMatrix, 0);
     System.arraycopy(temp, 0, projectionMatrix, 0, temp.length);
-    System.arraycopy(temp, 0, backMatrix, 0, temp.length);
   }
 
   private final float[] temp = new float[16];
-  private float angle = 0f;
 
-  private void updateMatrix() {
-    angle -= 0.0005f;
-    setIdentityM(modelMatrix, 0);
-    rotateM(modelMatrix, 0, angle, 1f, 0.3f, 0.8f);
-
-    multiplyMM(temp, 0, projectionMatrix, 0, modelMatrix, 0);
-    System.arraycopy(temp, 0, projectionMatrix, 0, temp.length);
-  }
-
-  private void updateVertexData(float percent) {
-    for (int i = 0; i < 4; i++) {
-      for (int j = 0; j < 3; j++) {
-        int index = i * 3 + j;
-        int index2 = index + 12;
-
-        float base = 0.3f;
-
-        if (j == 0) {
-          outerPoints[index2] = backOuterPoints[index2] - (percent)  * 2;
-
-          outerPoints[index] = backOuterPoints[index] + percent * 0.5f;
-
-          innerPoints[index2] = backInnerPoints[index2] + percent * 0.5f;
-          innerPoints[index] = backInnerPoints[index] + percent;
-        } else {
-          float scale = 1.2f;
-          if (percent < base) {
-            outerPoints[index2] = backOuterPoints[index2] * (1 + percent / base * (scale - 1));
-          } else {
-            outerPoints[index2] =
-                backOuterPoints[index2] * (scale + (percent - base) / (1 - base) * (1 - scale));
-          }
-
-          innerPoints[index2] = backInnerPoints[index2] * (1f + percent);
-
-          outerPoints[index] = backOuterPoints[index] * (1 - 0.5f * percent);
-        }
-      }
-    }
-
-    fillLines();
-    vertexData.put(lines);
+  private void updateNew(float percent) {
+    projection(percent);
+    vertexData.put(newLines);
     vertexData.position(0);
-
-    setIdentityM(modelMatrix, 0);
-    rotateM(modelMatrix, 0, -percent * 90, 1f, 0.f, 0.f);
-
-    multiplyMM(temp, 0, backMatrix, 0, modelMatrix, 0);
-    System.arraycopy(temp, 0, projectionMatrix, 0, temp.length);
   }
 
   private float percent = 0f;
-  private int loopCount = 0;
 
   @Override public void onDrawFrame(GL10 gl) {
-    if (percent >= 1f) {
-      percent = 0f;
-      loopCount++;
-    }
-    updateVertexData(percent);
-    percent += 0.009;
-
-    //updateMatrix();
+    percent += 0.015;
+    updateNew(percent);
 
     glClear(GLES20.GL_COLOR_BUFFER_BIT);
     glUniformMatrix4fv(uMatrixLocation, 1, false, projectionMatrix, 0);
 
-    //glEnable(GL_BLEND);
-    //glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    //
-    //glUniform4f(uColorLocation, 0.0f, 1.0f, 0.0f, 0.5f);
-    //glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-    //
-    //glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 0.5f);
-    //glDrawArrays(GL_TRIANGLE_STRIP, 4, 4);
-    //
-    //glUniform4f(uColorLocation, 1.0f, 1.0f, 0.0f, 0.5f);
-    //glDrawArrays(GL_TRIANGLE_STRIP, 8, 4);
-    //
-    //glUniform4f(uColorLocation, 1.0f, 0.0f, 1.0f, 0.5f);
-    //glDrawArrays(GL_TRIANGLE_STRIP, 12, 4);
-    //
-    //glUniform4f(uColorLocation, 0.0f, 1.0f, 1.0f, 0.5f);
-    //glDrawArrays(GL_TRIANGLE_STRIP, 16, 4);
-    //
-    //glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 0.5f);
-    //glDrawArrays(GL_TRIANGLE_STRIP, 20, 4);
-
-    glLineWidth(9.0f);
-    if (loopCount % 4 == 0) {
-      glUniform4f(uColorLocation, 1.0f, 1.0f, 0.0f, 1.0f);
-      glDrawArrays(GL_LINE_LOOP, 0, 4);
-
-      glUniform4f(uColorLocation, 1.0f, 0.0f, 1.0f, 1.0f);
-      glDrawArrays(GL_LINE_LOOP, 16, 4);
-
-      glUniform4f(uColorLocation, .8f, 0.5f, .0f, 1.0f);
-      glDrawArrays(GL_LINE_LOOP, 20, 4);
-
-      glUniform4f(uColorLocation, 0.6f, .2f, 0.7f, 1.0f);
-      glDrawArrays(GL_LINE_LOOP, 4, 4);
-
-      glUniform4f(uColorLocation, 0.7f, .6f, 0.3f, 1.0f);
-      glDrawArrays(GL_LINES, 8, 8);
-
-      glUniform4f(uColorLocation, 0.0f, 1.0f, 1.0f, 1.0f);
-      glDrawArrays(GL_LINES, 32, 8);
-
-      glUniform4f(uColorLocation, .2f, 0.9f, .3f, 1.0f);
-      glDrawArrays(GL_LINES, 24, 8);
-
-      glUniform4f(uColorLocation, 0.0f, 1.0f, 1.0f, 1.0f);
-      glDrawArrays(GL_LINES, 40, 8);
-
-    } else if (loopCount % 4 == 1) {
-      glUniform4f(uColorLocation, 0.6f, .2f, 0.7f, 1.0f);
-      glDrawArrays(GL_LINE_LOOP, 0, 4);
-
-      glUniform4f(uColorLocation, 1.0f, 1.0f, 0.0f, 1.0f);
-      glDrawArrays(GL_LINE_LOOP, 16, 4);
-
-      glUniform4f(uColorLocation, 1.0f, 0.0f, 1.0f, 1.0f);
-      glDrawArrays(GL_LINE_LOOP, 20, 4);
-
-      glUniform4f(uColorLocation, .8f, 0.5f, .0f, 1.0f);
-      glDrawArrays(GL_LINE_LOOP, 4, 4);
-
-      glUniform4f(uColorLocation, 0.0f, 1.0f, 1.0f, 1.0f);
-      glDrawArrays(GL_LINES, 8, 8);
-
-      glUniform4f(uColorLocation, 0.7f, .6f, 0.3f, 1.0f);
-      glDrawArrays(GL_LINES, 32, 8);
-
-      glUniform4f(uColorLocation, 0.0f, 1.0f, 1.0f, 1.0f);
-      glDrawArrays(GL_LINES, 24, 8);
-
-      glUniform4f(uColorLocation, .2f, 0.9f, .3f, 1.0f);
-      glDrawArrays(GL_LINES, 40, 8);
-
-    } else if (loopCount % 4 == 2) {
-      glUniform4f(uColorLocation, .8f, 0.5f, .0f, 1.0f);
-      glDrawArrays(GL_LINE_LOOP, 0, 4);
-
-      glUniform4f(uColorLocation, 0.6f, .2f, 0.7f, 1.0f);
-      glDrawArrays(GL_LINE_LOOP, 16, 4);
-
-      glUniform4f(uColorLocation, 1.0f, 1.0f, 0.0f, 1.0f);
-      glDrawArrays(GL_LINE_LOOP, 20, 4);
-
-      glUniform4f(uColorLocation, 1.0f, 0.0f, 1.0f, 1.0f);
-      glDrawArrays(GL_LINE_LOOP, 4, 4);
-
-      glUniform4f(uColorLocation, .2f, 0.9f, .3f, 1.0f);
-      glDrawArrays(GL_LINES, 8, 8);
-
-      glUniform4f(uColorLocation, 0.0f, 1.0f, 1.0f, 1.0f);
-      glDrawArrays(GL_LINES, 32, 8);
-
-      glUniform4f(uColorLocation, 0.7f, .6f, 0.3f, 1.0f);
-      glDrawArrays(GL_LINES, 24, 8);
-
-      glUniform4f(uColorLocation, 0.0f, 1.0f, 1.0f, 1.0f);
-      glDrawArrays(GL_LINES, 40, 8);
-
-    } else if (loopCount % 4 == 3) {
-      glUniform4f(uColorLocation, 1.0f, 0.0f, 1.0f, 1.0f);
-      glDrawArrays(GL_LINE_LOOP, 0, 4);
-
-      glUniform4f(uColorLocation, .8f, 0.5f, .0f, 1.0f);
-      glDrawArrays(GL_LINE_LOOP, 16, 4);
-
-      glUniform4f(uColorLocation, 0.6f, .2f, 0.7f, 1.0f);
-      glDrawArrays(GL_LINE_LOOP, 20, 4);
-
-      glUniform4f(uColorLocation, 1.0f, 1.0f, 0.0f, 1.0f);
-      glDrawArrays(GL_LINE_LOOP, 4, 4);
-
-      glUniform4f(uColorLocation, 0.0f, 1.0f, 1.0f, 1.0f);
-      glDrawArrays(GL_LINES, 8, 8);
-
-      glUniform4f(uColorLocation, .2f, 0.9f, .3f, 1.0f);
-      glDrawArrays(GL_LINES, 32, 8);
-
-      glUniform4f(uColorLocation, 0.0f, 1.0f, 1.0f, 1.0f);
-      glDrawArrays(GL_LINES, 24, 8);
-
-      glUniform4f(uColorLocation, 0.7f, .6f, 0.3f, 1.0f);
-      glDrawArrays(GL_LINES, 40, 8);
-    }
-
-    //
-    //// Draw the first mallet blue.
-    //glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
-    //glDrawArrays(GL_POINTS, 8, 1);
-    //// Draw the second mallet red.
-    //glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
-    //glDrawArrays(GL_POINTS, 9, 1);
+    glLineWidth(5.0f);
+    //12
+    glUniform4f(uColorLocation, 1f, 0f, 0f, 1.0f);
+    glDrawArrays(GL_LINES, 0, 24);
+    //12
+    glUniform4f(uColorLocation, 0f, 1f, 0f, 1.0f);
+    glDrawArrays(GL_LINES, 24, 24);
+    //8
+    glLineWidth(2.0f);
+    glUniform4f(uColorLocation, 1f, 0f, 1f, 1.0f);
+    glDrawArrays(GL_LINES, 48, 16);
   }
 }
